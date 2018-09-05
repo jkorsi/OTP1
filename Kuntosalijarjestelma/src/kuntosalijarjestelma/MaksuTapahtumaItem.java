84b9a5fd384236b41691985660d68f92416658c2
Juho Suni <juho.suni@metropolia.fi>
2018-09-05 15:09:50 +0300

    Lisätty maksujärjestelmään liittyvät lista ja sen jäsenItemit.

---
+++ Kuntosalijarjestelma/src/kuntosalijarjestelma/MaksuTapahtumaItem.java
@@ -0,0 +1,19 @@
+package kuntosalijarjestelma;
+
+public class MaksuTapahtumaItem {
+
+	private int tapahtumaAika;
+
+	private int maara;
+
+	private int tuloVaiMeno;
+
+	private String kommentti;
+
+	private int viite;
+
+	private String maksuTapa;
+
+	private MaksuTapahtumaLista maksuTapahtumaLista;
+
+}
